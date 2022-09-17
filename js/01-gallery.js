bac2344a74633157c93c17e408971c4446596adc
Denisiimo <denischorny22@gmail.com>
2022-09-17 19:37:51 +0100

    hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const IMG_NODE = "IMG";
+const galleryRef = document.querySelector('.gallery');
+const cardCreateEvt = cardCreate(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', cardCreateEvt);
+galleryRef.addEventListener('click', ModalOpen);
 
-console.log(galleryItems);
+function ModalOpen(evt) {
+    evt.preventDefault();
+    const { nodeName, dataset } = evt.target;
+    nodeName === IMG_NODE && showModal(dataset.source);
+}
+
+function showModal(src) {
+    const modalClose = () => {};
+    const modalLayout = `<img src="${src}">`;
+    const modalOptions = {
+        onShow: () => {
+            document.addEventListener("keydown", modalClose);
+        },
+        onClose: () => {
+            document.removeEventListener("keydown", modalClose);
+        },
+    };
+    const modal = basicLightbox.create(modalLayout, modalOptions)
+    modal.show();
+}
+function cardCreate(galleryItems){
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
+    }).join('');
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
+const itemCreate = cardCreate(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', itemCreate);
 // Change code below this line
 
-console.log(galleryItems);
+const simple = new SimpleLightbox(".gallery a", {captionDelay: 250, CaptionsData: "alt"});
+
+function cardCreate(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`
+    }).join(''); 
+}
+
